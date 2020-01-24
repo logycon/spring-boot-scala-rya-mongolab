932fe95c03a0db0870fe9530612788204a2cf855
kgu87 <igor@logycon.com>
2020-01-24 10:09:06 -0500

    response errors

--- src/main/scala/com/logycon/controllers/SparqlController.scala
+++ src/main/scala/com/logycon/controllers/SparqlController.scala
@@ -1,5 +1,7 @@
 package com.logycon.controllers
 
+import java.io.OutputStream
+
 import com.logycon.ryastore.SparqlOps
 import javax.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.{RequestBody, RequestHeader, Requ
 @ComponentScan
 class SparqlController @Autowired() (val sparql: SparqlOps) {
 
-  private def executeSparql(contentType: String, query: String, resp: HttpServletResponse): Unit = {
+  private def executeSparql(contentType: String, query: String, outputStream: OutputStream): Unit = {
 
     def getQueryType(query: String): String = {
       query match {
@@ -25,39 +27,29 @@ class SparqlController @Autowired() (val sparql: SparqlOps) {
       }
     }
 
-    resp.setHeader("content-type", contentType)
     val queryType = getQueryType(query)
     try {
       queryType match {
         case "select" => {
-          sparql.performSelect(query, resp.getOutputStream, contentType)
-          resp.setStatus(200)
+          sparql.performSelect(query, outputStream, contentType)
         }
 
         case "construct" => {
-          sparql.performConstruct(query, resp.getOutputStream, contentType)
-          resp.setStatus(200)
+          sparql.performConstruct(query, outputStream, contentType)
         }
 
         case "exec" => {
           sparql.performExec(query)
-          resp.setStatus(200)
         }
 
         case _ => {
-          val writer = resp.getWriter
-          writer.write(s"Invalid query ${query}")
-          writer.flush()
-          resp.setStatus(500)
+          outputStream.write(s"Invalid query ${query}".getBytes())
         }
 
       }
     } catch {
       case ex: Exception => {
-        resp.setStatus(500)
-        val writer = resp.getWriter
-        writer.write(s"Error running \n\n$query \n\nError: ${ex.getMessage}")
-        writer.flush()
+        outputStream.write(s"Error running \n\n$query \n\nError: ${ex.getMessage}".getBytes())
       }
     }
 
@@ -65,10 +57,13 @@ class SparqlController @Autowired() (val sparql: SparqlOps) {
 
   @RequestMapping(path = Array("sparql"), method = Array(RequestMethod.GET))
   def getSparql(
-    @RequestHeader(required = true, defaultValue = "application/json") contentType: String,
+    @RequestHeader(required = true, defaultValue = "application/json") accept: String,
     @RequestParam(required = true) query: String,
-    resp: HttpServletResponse): Unit = {
-    executeSparql(contentType, query, resp)
+    resp: HttpServletResponse): ResponseEntity[Void] = {
+
+    resp.setHeader("content-type", accept)
+    executeSparql(accept, query, resp.getOutputStream)
+    ResponseEntity.ok().build();
   }
 
   @RequestMapping(path = Array("sparql"),
@@ -78,8 +73,11 @@ class SparqlController @Autowired() (val sparql: SparqlOps) {
   def postSparql(
     @RequestHeader(required = true, name = "Accept", defaultValue = "application/json") accept: String,
     @RequestBody(required = true) query: String,
-    resp: HttpServletResponse): Unit = {
-    executeSparql(accept, query, resp)
+    resp: HttpServletResponse): ResponseEntity[Void] = {
+
+    resp.setHeader("content-type", accept)
+    executeSparql(accept, query, resp.getOutputStream)
+    ResponseEntity.ok().build();
   }
 
   @RequestMapping(path = Array("sparql"),
@@ -89,10 +87,12 @@ class SparqlController @Autowired() (val sparql: SparqlOps) {
   def postForm(
     @RequestHeader(required = true, name = "Accept", defaultValue = "application/json") accept: String,
     @RequestBody(required = true) query: MultiValueMap[String, String],
-    resp: HttpServletResponse): Unit = {
+    resp: HttpServletResponse): ResponseEntity[Void] = {
+
     val q: String = query.getFirst("query")
     val resType = accept.split(",")(0)
-    executeSparql(resType, q, resp)
+    executeSparql(resType, q, resp.getOutputStream)
+    ResponseEntity.ok().build();
   }
 
 }
