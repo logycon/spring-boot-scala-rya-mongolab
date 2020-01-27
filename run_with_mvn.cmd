32650e5d6750d3b0a7009a38058b5471e6b26db1
kgu87 <igor@logycon.com>
2020-01-27 05:22:16 -0500

    upating run.cmd

---
+++ run_with_mvn.cmd
@@ -0,0 +1,9 @@
+echo on
+set HADOOP_HOME=%cd%\misc\hadoop
+set spring.data.mongodb.uri=mongodb://localhost:27017
+set spring.data.mongodb.authentication-database=none
+set spring.data.mongodb.username=none
+set spring.data.mongodb.password=none
+set spring.data.mongodb.database=ryadb
+
+mvn spring-boot:run
